17759de389ef814ddf808b40b5c5c1860b09f1ce
techn1kk <thetechn1kk@gmail.com>
2022-07-19 20:54:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('sssss')
+def count_sheeps(sheep):
+  # TODO May the force be with you
+    g = 0
+    for element in sheep:
+        if element == True:
+            print(element)
+            g = g +1
+    return g
+
+
+array1 = [True,  True,  True,  False,
+        True,  True,  True,  True ,
+        True,  False, True,  False,
+        True,  False, False, True ,
+        True,  True,  True,  True ,
+        False, False, True,  True, True ]
+
+print(count_sheeps(array1))
